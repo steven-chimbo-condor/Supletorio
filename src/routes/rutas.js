66cd6b0054b42bebba3cc78dbeb422ba2a8aa296
steven-chimbo-condor <stevenchimboc@gmail.com>
2020-04-17 16:40:10 -0500

    suple

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -5,14 +5,13 @@ const express = require('express'),
 
     Cliente= require('../Controlador/controladorCliente');
 
-    Vehiculo= require('../Controlador/controladorvehiculo');
+  
 
 
 rutas.use('/obtener',Login);
 
 rutas.use('/mostrar',Cliente);
 
-rutas.use('/ver',Vehiculo);
 
 
 module.exports=rutas;
